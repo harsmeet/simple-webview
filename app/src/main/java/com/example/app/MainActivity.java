277199e39b6345b09ede3bd5e161a724de6fa05a
harsmeet <er.harsmeet@gmail.com>
2018-08-13 17:08:12 +0530

    add login html page

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends Activity {
          mWebView.setWebViewClient(new MyWebViewClient());
 
         // LOCAL RESOURCE
-         mWebView.loadUrl("file:///android_asset/index.html");
+         mWebView.loadUrl("file:///android_asset/login.html");
     }
 
     // Prevent the back-button from closing the app  ....
--- app/src/main/java/com/example/app/MyWebViewClient.java
+++ app/src/main/java/com/example/app/MyWebViewClient.java
@@ -2,13 +2,14 @@ package com.example.app;
 import android.content.Intent;
 import android.net.Uri;
 import android.webkit.WebView;
+
 import android.webkit.WebViewClient;
 
 public class MyWebViewClient extends WebViewClient {
 
     @Override
     public boolean shouldOverrideUrlLoading(WebView view, String url) {
-        if (Uri.parse(url).getHost().endsWith("file:///android_asset/index.html")) {
+        if (Uri.parse(url).getHost().endsWith("file:///android_asset/login.html")) {
             return false;
         }
 
